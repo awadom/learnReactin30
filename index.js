882c63495d88e00dd148b2b25f39a292009c7624
denfushi <awadomar@gmail.com>
2022-11-29 09:27:56 -0800

    const page

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const h1 = document.createElement("h1");
-h1.textContent = "Hello, React!";
-h1.className = "header";
-document.getElementById("root").append(h1);
+// const h1 = document.createElement("h1");
+// h1.textContent = "Hello, React!";
+// h1.className = "header";
+// console.log(h1);
+
+//JSX
+
+const page = (
+  <div>
+    <h1 className="header">This is JSX</h1>
+    <p>Paragraph</p>
+  </div>
+);
+
+console.log(page);
+
+ReactDOM.render(page, document.getElementById("root"));
