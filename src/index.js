accdb3a37d983028f180674234fc7003417bab3a
denfushi <awadomar@gmail.com>
2022-11-29 11:28:37 -0800

    basic static site started

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const page = (
     <h1>My awesome website in React</h1>
     <h3>Why I love React</h3>
     <ul>
-      <li>Its Compostable</li>
-      <li>Its declarative</li>
-      <li>Its a hireable skill</li>
-      <li>Its actively maintained by skilled people</li>
+      <li>It's Compostable</li>
+      <li>It's Declarative</li>
+      <li>It's a Hireable Skill</li>
+      <li>It's Actively Maintained by Skilled People</li>
     </ul>
   </div>
 );
 
-document.getElementById("root").append(page);
-// ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(page, document.getElementById("root"));
