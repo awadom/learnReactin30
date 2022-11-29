3f709394c52755c8b1eb8d429ac7dc381d961986
denfushi <awadomar@gmail.com>
2022-11-29 07:38:12 -0800

    ul list reactdom render practice

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>hi</h1>, document.getElementById("root"))
+ReactDOM.render(
+  <ul>
+    <li>
+      <h1>1</h1>
+    </li>
+    <li>
+      <h1>2</h1>
+    </li>
+    <li>
+      <h1>3</h1>
+    </li>
+  </ul>,
+document.getElementById("root")
+)
