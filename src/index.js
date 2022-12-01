63a9be4715dbf6eed4e4001832ba852423370246
denfushi <awadomar@gmail.com>
2022-12-01 14:48:38 -0800

    learned about className and added styling

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function Header() {
   return (
     <header>
-      <nav>
-        <img src="logo192.png" alt="" width="50px"></img>
+      <nav className="nav">
+        <img className="nav-logo" src="logo192.png" alt=""></img>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </header>
   );
 }
 function Content() {
   return (
-    <>
+    <div className="content">
       <h1>Why I want to learn React:</h1>
       <ol>
-        <li>To increase my development knowledge</li>
-        <li>To build my app</li>
-        <li>for fun!</li>
+        <li>To increase my development knowledge.</li>
+        <li>To build my app.</li>
+        <li>For fun!</li>
       </ol>
-    </>
+    </div>
   );
 }
 function Footer() {
   return (
-    <footer>
-      <small>c 2022 Awad development. All rights reserved.</small>
+    <footer className="footer">
+      <small>© 2022 Awad development. All rights reserved.</small>
     </footer>
   );
 }
 function Component() {
   return (
-    <>
+    <body className="component">
       <Header />
       <Content />
       <Footer />
-    </>
+    </body>
   );
 }
 
