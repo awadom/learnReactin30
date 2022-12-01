0b5f042e33f5e584efea4b437544f3f11ce01919
denfushi <awadomar@gmail.com>
2022-12-01 12:53:04 -0800

    header content and footer as components

--- src/index.js
+++ src/index.js
@@ -9,20 +9,31 @@ function Header() {
     </header>
   );
 }
-
-function Component() {
+function Content() {
   return (
     <>
-      <Header />
       <h1>Why I want to learn React:</h1>
       <ol>
         <li>To increase my development knowledge</li>
         <li>To build my app</li>
         <li>for fun!</li>
       </ol>
-      <footer>
-        <small>c 2022 Awad development. All rights reserved.</small>
-      </footer>
+    </>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <small>c 2022 Awad development. All rights reserved.</small>
+    </footer>
+  );
+}
+function Component() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
