dd7b51e86c1f24378fffbf18089e1200bd02b7fa
denfushi <awadomar@gmail.com>
2022-12-02 08:25:28 -0800

    created a component for App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <Content />
+      <Footer />
+    </main>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,47 +1,5 @@
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img className="nav-logo" src="logo192.png" alt=""></img>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-function Content() {
-  return (
-    <div className="content">
-      <h1>Why I want to learn React:</h1>
-      <ol>
-        <li>To increase my development knowledge.</li>
-        <li>To build my app.</li>
-        <li>For fun!</li>
-      </ol>
-    </div>
-  );
-}
-function Footer() {
-  return (
-    <footer className="footer">
-      <small>© 2022 Awad development. All rights reserved.</small>
-    </footer>
-  );
-}
-function Component() {
-  return (
-    <main>
-      <Header />
-      <Content />
-      <Footer />
-    </main>
-  );
-}
-
-ReactDOM.render(<Component />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
