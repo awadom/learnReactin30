3a54dad2368526422b9a06dda135aa465a42454a
denfushi <awadomar@gmail.com>
2022-12-02 22:29:49 -0800

    css styling all by myself

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,19 @@
 export default function Main() {
-  return <div>Main Component</div>;
+  return (
+    <>
+      <div className="main">
+        <div className="main-textpart">
+          <h1 className="main-h1">Fun facts about React</h1>
+          <ul className="main-ul">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </div>
+        <img className="main-logo" src="logo512.png" alt=""></img>
+      </div>
+    </>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,9 @@
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <div className="navbar">
+      <img className="navbar-logo" src="logo192.png" alt=""></img>
+      <h2 className="navbar-h2">ReactFacts</h2>
+      <h3 className="navbar-h3">React Course - Project 1</h3>
+    </div>
+  );
 }
